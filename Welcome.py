4b4822e7543f8ac85ae664f3d9389c0b82660aa8
darwinleung <darwinlkh@gmail.com>
2024-09-16 23:44:26 -0400

    auto size img on welcome.py

--- Welcome.py
+++ Welcome.py
@@ -5,4 +5,4 @@ st.set_page_config(page_title="PTCG Calculators", page_icon="img/favicon.ico")
 st.markdown("# Welcome to PokemonTCG Toolbox!")
 st.markdown("Here you can find some useful tools for playing PTCG, give them a try!")
 
-st.image("img/pokemon-pokedex-anime.webp", width = 600)
+st.image("img/pokemon-pokedex-anime.webp")
